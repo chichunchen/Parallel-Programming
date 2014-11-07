210c195258c5fc74c9df0a990b75f2b9884b804e
chi chun chen <chi.chun_chen844@icloud.com>
2014-11-07 14:11:11 +0800

    hw1 turn to time calc mode

--- HW1/PiC/hw1_pthread.c
+++ HW1/PiC/hw1_pthread.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define THREAD_NUM 8
 typedef unsigned long long int unlong; 
 
 unlong number_in_circle = 0;
@@ -34,21 +33,20 @@ void *runner() {
 
 int main(int argc, const char *argv[])
 {
-    if (argc != 2) {
-        fprintf(stderr, "usage: ./pi <number of tosses>\n");
+    if (argc != 3) {
+        fprintf(stderr, "usage: ./pi <number of tosses> <threads>\n");
         exit(1);
     }
 
     unlong number_of_tosses = atoll(argv[1]);
-    // int thread_count = atoi(argv[2]);
-    int thread_count = THREAD_NUM;
+    int thread_count = atoi(argv[2]);
     points_per_thread = number_of_tosses / thread_count;
 
     /* Tried using clock, but it measures CPU time, not wall clock time,
      * so doesn't demonstrate improvement gained by threading */
-    // time_t start = time(NULL);
+    time_t start = time(NULL);
 
-    // srand((unsigned)time(NULL));
+    srand((unsigned)time(NULL));
 
     pthread_t *threads = malloc(thread_count * sizeof(pthread_t));
 
@@ -70,7 +68,7 @@ int main(int argc, const char *argv[])
     double pi_estimate = (4. * (unlong)number_in_circle) / ((unlong)points_per_thread * thread_count); 
 
     printf("%f\n", pi_estimate);
-    // printf("Time: %d sec\n", (int)(time(NULL) - start));
+    printf("Time: %d sec\n", (int)(time(NULL) - start));
 
     return 0;
 }
